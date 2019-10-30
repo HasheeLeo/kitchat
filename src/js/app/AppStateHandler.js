c1aed314278f8ddeb7589f6d621ec309b85d2fd1
Hashir Ahmad <hashir@hashirahmad.com>
2019-10-30 23:30:51 +0500

    Initial PersistentTask implementation

--- src/js/app/AppStateHandler.js
+++ src/js/app/AppStateHandler.js
@@ -1,13 +1,22 @@
 // @flow
 
+import uuidv4 from 'uuid/v4';
+
 import {AppState} from 'react-native';
 import SessionFactory from '~/app/SessionFactory';
 
+type callbacks = {|
+  onActive?: () => Promise<any>,
+  onInActive?: () => Promise<any>
+|};
+
 class AppStateHandler {
   static appState: ?string;
+  static subscribers: Array<{id: string, ...callbacks}>;
 
   static init() {
     AppStateHandler.appState = AppState.currentState;
+    AppStateHandler.subscribers = [];
     AppState.addEventListener('change', AppStateHandler.handle);
   }
 
@@ -15,15 +24,39 @@ class AppStateHandler {
     AppState.removeEventListener('change', AppStateHandler.handle);
   }
 
+  static subscribe(callbacks: callbacks): any {
+    const id = uuidv4();
+    AppStateHandler.subscribers.push({
+      id: id,
+      ...callbacks
+    });
+
+    return () => (
+      AppStateHandler.subscribers = AppStateHandler.subscribers.filter(
+        subscriber => subscriber.id !== id
+      )
+    );
+  }
+
   static getAppState() {
     return AppStateHandler.appState;
   }
 
   static handle(nextAppState: string) {
-    if (nextAppState === 'active')
+    if (nextAppState === 'active') {
       SessionFactory.resumeSession();
-    else
+      AppStateHandler.subscribers.forEach(subscriber => {
+        if (subscriber.onActive)
+          subscriber.onActive();
+      });
+    }
+    else {
       SessionFactory.pauseSession();
+      AppStateHandler.subscribers.forEach(subscriber => {
+        if (subscriber.onInActive)
+          subscriber.onInActive();
+      });
+    }
   }
 }
 
